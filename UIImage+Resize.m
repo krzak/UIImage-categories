3dccbe11f324721c3a98a0d32ca9ff9560491aae
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2012-06-17 23:15:39 +0200

    remove warnings

--- UIImage+Resize.m
+++ UIImage+Resize.m
@@ -158,6 +158,8 @@
             transform = CGAffineTransformTranslate(transform, 0, newSize.height);
             transform = CGAffineTransformRotate(transform, -M_PI_2);
             break;
+        default:
+            break;
     }
     
     switch (self.imageOrientation) {
@@ -171,6 +173,8 @@
         case UIImageOrientationRightMirrored:  // EXIF = 7
             transform = CGAffineTransformTranslate(transform, newSize.height, 0);
             transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        default:
             break;
     }
     
